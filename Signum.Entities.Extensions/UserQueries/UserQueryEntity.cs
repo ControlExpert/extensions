d0f8f357a93ad687fcbc0ab61cf76ac9c5d20b67
sschoensee <s.schoensee@controlexpert.com>
2026-05-05 10:11:09 +0200

    COM-8026 Removes EasyClaim specific changes UserQueryEntity.cs HiddenColumn

--- Signum.Entities.Extensions/UserQueries/UserQueryEntity.cs
+++ Signum.Entities.Extensions/UserQueries/UserQueryEntity.cs
@@ -222,15 +222,12 @@ namespace Signum.Entities.UserQueries
 
         public QueryTokenEmbedded? SummaryToken { get; set; }
 
-        public bool HiddenColumn { get; set; }
-
         public XElement ToXml(IToXmlContext ctx)
         {
             return new XElement("Column",
                 new XAttribute("Token", Token.Token.FullKey()),
                 SummaryToken != null ? new XAttribute("SummaryToken", SummaryToken.Token.FullKey()) : null!,
-                DisplayName.HasText() ? new XAttribute("DisplayName", DisplayName) : null!,
-                HiddenColumn ? new XAttribute("HiddenColumn", HiddenColumn) : null!);
+                DisplayName.HasText() ? new XAttribute("DisplayName", DisplayName) : null!);
         }
 
         internal void FromXml(XElement element, IFromXmlContext ctx)
@@ -238,7 +235,6 @@ namespace Signum.Entities.UserQueries
             Token = new QueryTokenEmbedded(element.Attribute("Token")!.Value);
             SummaryToken = element.Attribute("SummaryToken")?.Value.Let(val => new QueryTokenEmbedded(val));
             DisplayName = element.Attribute("DisplayName")?.Value;
-            HiddenColumn = element.Attribute("HiddenColumn")?.Value.ToBool() == false;
         }
 
         public void ParseData(Entity context, QueryDescription description, SubTokensOptions options)
